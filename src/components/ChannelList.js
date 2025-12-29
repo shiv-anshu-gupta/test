3ed6921af737cff175e55001a4de55b926a2faa9
shiv-anshu-gupta <shivanshu2019gupta@gmail.com>
2025-12-29 13:04:12 +0530

    temp

--- src/components/ChannelList.js
+++ src/components/ChannelList.js
@@ -1819,7 +1819,7 @@ export function createChannelList(
       id: i + 1,
       channelID: ch.channelID,
       originalIndex: i,
-      type: "Computed",
+      type: "Analog",
       name: ch.name || ch.id || `Computed ${i + 1}`,
       unit: ch.unit || "",
       group:
@@ -1827,8 +1827,8 @@ export function createChannelList(
         typeof ch.group === "string" &&
         ch.group.startsWith("G")
           ? ch.group
-          : "G9",
-      color: ch.color || "#FF6B6B",
+          : "G10",
+      color: ch.color || "#4ECDC4",
       scale: ch.scale || 1,
       start: ch.start || 0,
       duration: ch.duration || "",
@@ -2446,10 +2446,10 @@ export function createChannelList(
 
               const newRow = {
                 id: nextComputedId,
-                type: "Computed",
+                type: "Analog",
                 name: channelName,
                 unit: "",
-                group: "Computed",
+                group: "G10",
                 color: "#888",
                 scale: 1,
                 start: 0,
@@ -2551,11 +2551,11 @@ export function createChannelList(
                 cfg.computedChannels.length,
               channelID: computedCh.channelID,
               originalIndex: cfg.computedChannels.length - 1,
-              type: "Computed",
+              type: "Analog",
               name: computedCh.id || `Computed ${cfg.computedChannels.length}`,
               unit: computedCh.unit || "",
-              group: "Computed Channels",
-              color: computedCh.color || "#FF6B6B",
+              group: "G10",
+              color: computedCh.color || "#4ECDC4",
               scale: computedCh.scale || 1,
               start: computedCh.start || 0,
               duration: computedCh.duration || "",
